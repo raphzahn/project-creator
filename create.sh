edf137f305cad2ec5c4f09509c4a97f249934908
Raphael Zahner <r.zahner@voegeli.ch>
2021-02-10 00:19:15 +0100

    update create.sh

--- create.sh
+++ create.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 function create(){
-    echo $2
     mkdir $1
     cd $1
-    git init
+
     if [ $2 == "--web" ]
     then
         npm init -y
-        npx license MIT
     fi
+
+    npx license MIT
+    echo "# $1">>README.md
+
+    git init
+    git add *
+    git commit -m ":sparkles: inital commit"
+    
 }
