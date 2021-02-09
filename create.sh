2670249b01ddfe97d391ee0236d96898199b8ff8
Raphael Zahner <r.zahner@voegeli.ch>
2021-02-10 00:50:53 +0100

    add Github repo creation

--- create.sh
+++ create.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+USERNAME=
+ACCESS_TOKEN=
+
 function create(){
     mkdir $1
     cd $1
@@ -12,8 +15,12 @@ function create(){
     npx license MIT
     echo "# $1">>README.md
 
+    curl -u "$USERNAME:$ACCESS_TOKEN" https://api.github.com/user/repos -d '{"name":"'$1'"}'
+
     git init
     git add *
     git commit -m ":sparkles: inital commit"
+    git remote add origin https://github.com/$USERNAME/$1.git
+    git push -u origin master
     
 }
